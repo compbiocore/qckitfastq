a14cc6c7d133ac36bea3ec0714ba05aeedcf8a6b
winnie0521 <wenyue.xing@brown.edu>
2018-05-08 16:51:07 -0400

    sequence content add count

--- R/sequence_content.R
+++ R/sequence_content.R
@@ -2,6 +2,7 @@
 #' @param fseq an object that is the read result from seq.read function
 #' @param content an object of string type that specifies the content in question, "A","T","G","C","N"(either capital or lower case)
 #' @param writefile boolean true/false for whether to write file
+#' @param prefix to the output file if writefile=TRUE
 #' @return the per position
 #' @examples
 #' infile <- system.file("extdata", "10^5_reads_test.fq.gz", package = "qckitfastq")
@@ -10,7 +11,7 @@
 #' @author Wenyue Xing, \email{wenyue_xing@@brown.edu}
 #' @export
 
-sequence_content <- function(fseq,content,writefile=FALSE){
+sequence_content <- function(fseq,content,writefile=FALSE,prefix){
 
   nucCount_seq <- seqTools::nucFreq(fseq,1)
   specific_content <- nucCount_seq[tolower(content),]
