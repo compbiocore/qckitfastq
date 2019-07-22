3c6e827c96a137421ba48d02ec887c5abee23aed
August Guang <august.guang@gmail.com>
2019-07-22 14:27:09 -0400

    added aes group

--- R/plot_per_base_quality.R
+++ R/plot_per_base_quality.R
@@ -10,17 +10,16 @@
 #' @return A boxplot of per position quality score distribution.
 #' @importFrom rlang .data
 plot_per_base_quality <- function(per_base_quality, output_file=NA){
-
-  per_base_quality$index = seq(1,nrow(per_base_quality),1)
+    
   # To address R CMD check from giving a NOTE about undefined global variables
   # https://dplyr.tidyverse.org/articles/programming.html
-  p_quality_score <- ggplot2::ggplot(data=per_base_quality,aes(x=.data$index)) +
+  p_quality_score <- ggplot2::ggplot(data=per_base_quality,aes(x=.data$position)) +
     ggplot2::labs(x = "Position", y = "Quality Score",title = "Quality score distribution per position") +
     ggplot2::geom_rect(aes(ymin=28,ymax=Inf,xmin=-Inf,xmax=Inf),fill='lightgreen',alpha=.02) +
     ggplot2::geom_rect(aes(ymin=20,ymax=28,xmin=-Inf,xmax=Inf),fill='yellow',alpha=.02) +
     ggplot2::geom_rect(aes(ymin=-Inf,ymax=20,xmin=-Inf,xmax=Inf),fill='red',alpha=.01) +
     ggplot2::geom_boxplot(ggplot2::aes(ymin=.data$q10,lower=.data$q25,middle = .data$median,
-                                       upper = .data$q75,ymax=.data$q90),stat="identity")
+                                       upper = .data$q75,ymax=.data$q90,group=.data$position),stat="identity")
   
   if(!is.na(output_file)) {ggplot2::ggsave(file=output_file,p_quality_score)}
 
